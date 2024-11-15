8b13f879de18548ea98517e39b19bd36fa0cbfd3
Flynn Bundy <flynn.sbundy@gmail.com>
2024-11-15 15:10:00 +0100

    add ethglobal lin cdk init

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BackendStack } from '../lib/backend-stack';
+import 'dotenv/config';
+import 'source-map-support/register';
+import { EthGlobalBangkok2024Stack } from '../lib/eth-global-bangkok-2024-stack';
 
 const app = new cdk.App();
-new BackendStack(app, 'BackendStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new EthGlobalBangkok2024Stack(app, 'EthGlobalBangkok2024Stack', {
+  env: {
+    region: 'us-east-1',
+  }
 });

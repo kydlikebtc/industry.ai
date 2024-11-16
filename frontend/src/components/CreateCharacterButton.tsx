5ba675b9928bdcb933bc14296631abe68cb4cab3
Soheima Maradiaga <soheimam@gmail.com>
2024-11-16 11:05:48 +0700

    added shadcn button

--- frontend/src/components/CreateCharacterButton.tsx
+++ frontend/src/components/CreateCharacterButton.tsx
@@ -1,19 +1,25 @@
 'use client'
 import { pixelify_sans } from '@/app/fonts';
-
 import { useCharacterSelect } from '@/contexts/CharacterSelectContext';
 
 function CreateCharacterButton() {
-    const { setIsOpen } = useCharacterSelect()
+    const { setIsOpen } = useCharacterSelect();
+
+    const handleClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsOpen(true);
+    };
 
     return (
         <button
-            onClick={() => setIsOpen(true)}
-            className={`${pixelify_sans.className} bg-primary hover:bg-primary/90 text-white px-4 py-2 rounded`}
+            onClick={handleClick}
+            type="button"
+            className={`${pixelify_sans.className} bg-primary hover:bg-primary/90 text-white px-4 py-2 rounded cursor-pointer`}
         >
             Create Character
         </button>
-    )
+    );
 }
 
-export default CreateCharacterButton 
+export default CreateCharacterButton; 
